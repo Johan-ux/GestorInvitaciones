1e0eb5a5d661cb1106d809af3ff614f575778691
Johan-ux <johan.sebastian.franco2002@gmail.com>
2024-09-01 15:16:27 +0200

    api modificada otra vez 2

--- src/main/java/oso/dev/gestorinvitaciones/controller/ConfirmacionAsistenciaController.java
+++ src/main/java/oso/dev/gestorinvitaciones/controller/ConfirmacionAsistenciaController.java
@@ -1,7 +1,9 @@
 package oso.dev.gestorinvitaciones.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import oso.dev.gestorinvitaciones.documents.Asistencia;
 import oso.dev.gestorinvitaciones.service.AsistenciaService;
@@ -15,18 +17,18 @@ public class ConfirmacionAsistenciaController {
     private AsistenciaService asistenciaService;
 
     @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public void saveAsistencia(@RequestParam(required = false) Map<String, String> params) {
-
+    public ResponseEntity<String> saveAsistencia(@RequestParam(required = false) Map<String, String> params) {
         if (!params.isEmpty()) {
-
             Asistencia asistencia = new Asistencia();
-
             asistencia.setName(params.get("nameIv"));
             asistencia.setAsistencia("si".equals(params.get("asistencia")));
             asistencia.setMensajeOp(params.get("mensajeOp"));
 
             asistenciaService.save(asistencia);
+
+            return ResponseEntity.ok(asistencia.isAsistencia() ? "Asistencia confirmada" : "Inasistencia confirmada");
         }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Datos no válidos");
     }
 
     @Autowired
