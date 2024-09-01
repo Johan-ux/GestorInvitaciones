f9761d3cd879247198b7ce42c91da18098cfe983
Johan-ux <johan.sebastian.franco2002@gmail.com>
2024-09-01 14:58:50 +0200

    api modificada

--- src/main/java/oso/dev/gestorinvitaciones/controller/ConfirmacionAsistenciaController.java
+++ src/main/java/oso/dev/gestorinvitaciones/controller/ConfirmacionAsistenciaController.java
@@ -1,9 +1,12 @@
 package oso.dev.gestorinvitaciones.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import oso.dev.gestorinvitaciones.documents.Asistencia;
 import oso.dev.gestorinvitaciones.service.AsistenciaService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/asistencia", method = RequestMethod.POST)
@@ -11,9 +14,19 @@ public class ConfirmacionAsistenciaController {
 
     private AsistenciaService asistenciaService;
 
-    @PostMapping
-    public void saveAsistencia(@RequestBody Asistencia asistencia) {
-        asistenciaService.save(asistencia);
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void saveAsistencia(@RequestParam(required = false) Map<String, String> params) {
+
+        if (!params.isEmpty()) {
+
+            Asistencia asistencia = new Asistencia();
+
+            asistencia.setName(params.get("name"));
+            asistencia.setAsistencia("si".equals(params.get("asistencia")));
+            asistencia.setMensajeOp(params.get("mensajeOp"));
+
+            asistenciaService.save(asistencia);
+        }
     }
 
     @Autowired
